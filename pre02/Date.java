872a2e789810103004bfe720ae5e701294dc361e
AliseBraick <97761594+AliseBraick@users.noreply.github.com>
2022-01-15 22:07:00 +0000

    I did the Pre02. Print European date

---
+++ pre02/Date.java
@@ -0,0 +1,11 @@
+public class Date{
+ 
+  public static void main (String[]args){
+    String day = "Saturday";
+    String month = "Januaray";
+    int number = 17;
+    int year = 2022;
+    System.out.println(" Prework2");
+    System.out.println(day + " " + number + " " + month + " " + year);
+  }
+}
