f6c29b29da47787ea413e558c1d4aa491b95744e
AliseBraick <97761594+AliseBraick@users.noreply.github.com>
2022-01-17 17:58:28 +0000

    I completed chapter 4 (Void)

--- pre02/Guess.java
+++ pre02/Guess.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 import java.util.Random;
-public class Guess {
 
-  public static void main(String[] args){
+public class Guess {
+ public static void main(String[] args){
     Random random = new Random ();
     int number = random.nextInt(100)+1;
 
